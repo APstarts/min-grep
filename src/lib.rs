27f37e35d23f2c48c5fc6e79f26674c377823c57
Anurag Pal <tanmaypal123@outlook.com>
2026-07-01 15:36:39 +0530

    added pdf reading functionality

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use std::{
     path::Path,
 };
 
+use lopdf::Document;
+
 // pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 //     let mut results = Vec::new();
 //     for line in contents.lines() {
@@ -29,6 +31,26 @@ use std::{
 
 /// this function searches a file's content with the query provided and returns a bool, if found then true and if not found then false.
 pub fn search_file(file: &Path, query: &str) -> bool {
+    match file.extension().and_then(|e| e.to_str()) {
+        Some("txt") => search_txt_file(file, query),
+        Some("pdf") => search_pdf_file(file, query),
+        _ => false,
+    }
+}
+
+pub fn search_pdf_file(file: &Path, query: &str) -> bool {
+    let doc = Document::load(file).unwrap();
+    let pages = doc.get_pages();
+    for (&page_number, _) in &pages {
+        let text = doc.extract_text(&[page_number]).unwrap();
+        if text.contains(query) {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn search_txt_file(file: &Path, query: &str) -> bool {
     let f = File::open(file).expect("failed to open file");
     let reader = BufReader::new(f);
 
